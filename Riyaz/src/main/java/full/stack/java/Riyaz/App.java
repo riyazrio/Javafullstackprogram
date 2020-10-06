ea2baf225c0a49fd34a074c73e5939c8e214995e
riyazrio <riyazrio9717@gmail.com>
2020-10-06 23:25:29 +0530

    Introduction

---
+++ Riyaz/src/main/java/full/stack/java/Riyaz/App.java
@@ -0,0 +1,13 @@
+package full.stack.java.Riyaz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hiii Riyaz");
+    }
+}
